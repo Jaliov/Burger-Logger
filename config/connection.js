c8e788e7a05162ea40877af38b91110578bdd4b4
Joel Rudin <jarud55@gmail.com>
2020-03-22 20:14:38 -0400

    heroku

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "ko86t9azcob3a2f9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "Glottle5832",
-  database: "burgers_db"
+  user: "shw6rwp4ov5owukk",
+  password: "s7jh116weq8tqr1c",
+  database: "nq5upnezu6wegs52"
 });
 
 // Make connection.
@@ -18,5 +18,22 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
-module.exports = connection;
+// // Export connection for our ORM to use.
+ module.exports = connection;
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: 
+//   database: "burgers_db"
+// });
+
+// // Make connection.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
