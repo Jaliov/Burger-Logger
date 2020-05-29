cb02c220101ba8c0eef856b05fe1ad44025eacbf
Joel Rudin <jarud55@gmail.com>
2020-05-28 22:04:59 -0400

    styling edits

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,26 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
+var mysql = require('mysql');
 
-var connection = mysql.createConnection(process.env.NODE_ENV === "production" ? process.env.JAWSDB_URL : {
-  "user": "root",
-  "password": "root",
-  "database": "burgers_db",
-  "host": "localhost",
-  "port": 3306,
-});
+var connection = mysql.createConnection(
+  process.env.NODE_ENV === 'production'
+    ? process.env.JAWSDB_URL
+    : {
+        user: 'root',
+        password: 'Glottle5832',
+        database: 'burgers_db',
+        host: 'localhost',
+        port: 3306,
+      }
+);
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
-  console.error("error connecting: " + err.stack);
+    console.error('error connecting: ' + err.stack);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log('connected as id ' + connection.threadId);
 });
 
 // // Export connection for our ORM to use.
- module.exports = connection;
-
-
+module.exports = connection;
